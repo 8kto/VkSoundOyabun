564aaf1b178f5383d9a6b4c192a8c229d0171a55
Okto <zm@axisful.info>
2016-09-16 18:43:52 +0500

    Рабочий вариант

--- oyabun.py
+++ oyabun.py
@@ -3,27 +3,26 @@
 
 import vk_api
 import configparser
-from pprint import pprint
 import re
 import time
 import sys
+import argparse
+from pprint import pprint
 
-# Read config
-reader = config = configparser.ConfigParser()
-reader.read('config.ini')
-
-_user_id=reader.get('USER', 'id')
-_user_pass=reader.get('USER', 'pass')
-_user_login=reader.get('USER', 'login')
-
-_ini_file="./albums.ini"
+# Local vars
 keepcharacters = (' ','.','_')
-isVerbose = True
-sleep_every_tracknum = 800
 
 # Main process
-def main():
+def main(config_path, output_path, is_verbose=True, sleep_every_tracknum=200, pause_sec=15):
     """ Загрузка альбомов в ini-файл """
+
+    # Read config
+    reader = config = configparser.ConfigParser()
+    reader.read(config_path)
+
+    _user_id=reader.get('USER', 'id')
+    _user_pass=reader.get('USER', 'pass')
+    _user_login=reader.get('USER', 'login')
 
     vk_session = vk_api.VkApi(_user_login, _user_pass)
     config = configparser.ConfigParser()
@@ -36,42 +35,16 @@ def main():
         return
 
     vk = vk_session.get_api()
-    albums = vk.audio.getAlbums(owner_id=_user_id, count=2)
+    #albums = vk.audio.getAlbums(owner_id=_user_id, count=2)
+    albums = vk.audio.getAlbums(owner_id=_user_id)
 
     if (not albums):
         raise Exception('No albums loaded')
 
-    # Хранилище id => альбом
-    keys = [i['id'] for i in albums['items']]
-    albums_indexed = dict(zip(keys, albums['items']))
-
     # Подготовка файла
-    config.read(_ini_file)
-    cfgfile = open(_ini_file, 'w')
+    config.read(output_path)
+    cfgfile = open(output_path, 'w')
     ctn, file_count = 0, 0
-
-    tracks = vk.audio.get(owner_id=_user_id)
-    for track in tracks['items']:
-        trackname = "%s — %s" % (track['artist'], track['title'])
-        pprint (track)
-
-        sys.exit()
-        ctn += 1
-        if (sleep_every_tracknum < ctn):
-            print('Пауза...')
-            time.sleep(3)
-            ctn = 0
-
-        try:
-            #pprint (track)
-            config.set(album_sec, trackname, track['url'])
-            isVerbose and print(">> Добавлен %s/%s" % (atitle, trackname))
-            file_count += 1
-
-        except configparser.DuplicateSectionError as error_msg:
-            isVerbose and print(">> Пропуск %s/%s" % (atitle, trackname))
-            continue
-
 
     # Заполнить файл альбомами, которых ещё нет
     for album in albums['items']:
@@ -80,33 +53,65 @@ def main():
 
         try:
             config.add_section(album_sec)
-            isVerbose and print("> Добавлен %s" % atitle)
+            is_verbose and print("> Добавлен %s" % atitle)
 
         except configparser.DuplicateSectionError as error_msg:
-            isVerbose and print("> Пропуск %s" % atitle)
+            is_verbose and print("> Пропуск %s" % atitle)
 
         # Заполнить альбом треками
         try:
             tracks = vk.audio.get(owner_id=_user_id, album_id=aid)
+            for track in tracks['items']:
+                trackname = "%s — %s" % (track['artist'], track['title'])
+
+                # Пауза
+                ctn += 1
+                if (sleep_every_tracknum < ctn):
+                    print('Пауза %d секунд...' % pause_sec)
+                    time.sleep(pause_sec)
+                    ctn = 0
+
+                try:
+                    #pprint (track)
+                    config.set(album_sec, trackname, track['url'])
+                    is_verbose and print(">> Добавлен %s/%s" % (atitle, trackname))
+                    file_count += 1
+
+                except configparser.DuplicateSectionError as error_msg:
+                    is_verbose and print(">> Пропуск %s/%s" % (atitle, trackname))
+                    continue
 
         except vk_api.vk_api.Captcha:
             print("Сайт требует ввести капчу, работа прекращена (обработано %d файлов)" % file_count)
+            close_all(config, cfgfile, output_path)
+            return
 
-            if cfgfile.closed:
-                cfgfile = open(_ini_file, 'w')
-            config.write(cfgfile)
-            cfgfile.close()
+    close_all(config, cfgfile, output_path)
+    print("Обработано %d файлов" % file_count)
 
+    #if cfgfile.closed:
+        #cfgfile = open(output_path, 'w')
+    #config.write(cfgfile)
+    #cfgfile.close()
 
-
-    if cfgfile.closed:
-        cfgfile = open(_ini_file, 'w')
-    config.write(cfgfile)
-    cfgfile.close()
+def close_all(config, fh, fname):
+    """ Закрыть ридер и файл """
+    if fh.closed:
+        fh = open(output_path, 'w')
+    config.write(fh)
+    fh.close()
 
 # public static void
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", type=str, help="Файл с параметрами входа на ВК")
+    parser.add_argument("output", type=str, help="Куда сохранить результаты")
+    parser.add_argument("-v", "--verbose", help="Подробные сообщения", action="store_true", default=True)
+    parser.add_argument("-e", "--every", help="Пауза каждые n треков", type=int, default=200)
+    parser.add_argument("-p", "--pause", help="Продолжительность паузы", type=int, default=15)
+
+    args = parser.parse_args()
+    main(args.config, args.output, args.verbose, args.every, args.pause)
 
 #print("".join(c for c in album['atitle'] if c.isalnum() or c in keepcharacters).rstrip())
 
