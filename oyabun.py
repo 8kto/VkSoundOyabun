ca21c83dc679f51517a53db7a6a7ef025be4bb35
Okto <zm@axisful.info>
2016-09-16 23:35:52 +0500

    Многопоточное скачивание музыки

--- oyabun.py
+++ oyabun.py
@@ -5,19 +5,29 @@ import vk_api
 import configparser
 import re
 import time
-import sys
+import os, sys
 import argparse
+import html
+#from Downloada import Downloada
 from pprint import pprint
 
-# Local vars
-keepcharacters = (' ','.','_')
+import threading
+import queue
+from urllib.request import urlretrieve
 
-# Main process
-def main(config_path, output_path, is_verbose=True, sleep_every_tracknum=200, pause_sec=15):
+
+# Local vars
+keepcharacters = (' ','.','_', '—', '(', ')')
+
+#===============================================================================
+# NB: Максимальное число альбомов для запроса - 100
+# https://vk.com/dev/audio.getAlbums
+#===============================================================================
+def parse(config_path, output_path, is_verbose=True, sleep_every_tracknum=200, pause_sec=15):
     """ Загрузка альбомов в ini-файл """
 
     # Read config
-    reader = config = configparser.ConfigParser()
+    reader = configparser.ConfigParser()
     reader.read(config_path)
 
     _user_id=reader.get('USER', 'id')
@@ -35,8 +45,7 @@ def main(config_path, output_path, is_verbose=True, sleep_every_tracknum=200, pa
         return
 
     vk = vk_session.get_api()
-    #albums = vk.audio.getAlbums(owner_id=_user_id, count=2)
-    albums = vk.audio.getAlbums(owner_id=_user_id)
+    albums = vk.audio.getAlbums(owner_id=_user_id, count=100)
 
     if (not albums):
         raise Exception('No albums loaded')
@@ -49,7 +58,7 @@ def main(config_path, output_path, is_verbose=True, sleep_every_tracknum=200, pa
     # Заполнить файл альбомами, которых ещё нет
     for album in albums['items']:
         aid, atitle = album['id'], album['title']
-        album_sec = atitle #"%s | %s" % (atitle, aid)
+        album_sec = safe_fs_name(atitle) #"%s | %s" % (atitle, aid)
 
         try:
             config.add_section(album_sec)
@@ -73,7 +82,7 @@ def main(config_path, output_path, is_verbose=True, sleep_every_tracknum=200, pa
 
                 try:
                     #pprint (track)
-                    config.set(album_sec, trackname, track['url'])
+                    config.set(album_sec, safe_fs_name(trackname), track['url'])
                     is_verbose and print(">> Добавлен %s/%s" % (atitle, trackname))
                     file_count += 1
 
@@ -94,6 +103,7 @@ def main(config_path, output_path, is_verbose=True, sleep_every_tracknum=200, pa
     #config.write(cfgfile)
     #cfgfile.close()
 
+#===============================================================================
 def close_all(config, fh, fname):
     """ Закрыть ридер и файл """
     if fh.closed:
@@ -101,17 +111,113 @@ def close_all(config, fh, fname):
     config.write(fh)
     fh.close()
 
+#===============================================================================
+def download(config_path, output_path='./', is_verbose=True, threads_num=5, pause_sec=15):
+    """ Скачать файлы из форматированного файла """
+
+    # Read config
+    reader = configparser.ConfigParser()
+    reader.read(config_path)
+
+    for section in reader.sections():
+        dirname = "%s/%s" % (output_path, (safe_fs_name(section).title()))
+
+        #pprint (dirname)
+
+        # Создать директории-альбомы
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        init_threads(threads_num, dirname, dict(reader[section]))
+        #time.sleep(pause_sec)
+
+#===============================================================================
+def init_threads(tnum, output_path, data_list):
+    """ Запустить процессы на скачивание """
+
+    # Create the queue and thread pool
+    q = queue.Queue()
+    for i in range(tnum):
+         t = threading.Thread(target=down_worker, kwargs={'queue': q, 'output_path': output_path})
+         t.daemon = True  # thread dies when main thread (only non-daemon thread) exits.
+         t.start()
+
+    for item in data_list.items():
+        q.put(item)
+
+    # block until all tasks are done
+    q.join()
+
+#===============================================================================
+def down_worker(queue, output_path):
+    """ Скачивающий процесс """
+    #title, url = queue.get()
+    #fpath = '%s/%s.mp3' % (output_path, title)
+    #print(fpath)
+    while True:
+        #pprint(queue.get())
+        title, url = queue.get()
+        title = title.title() # I am sorry
+
+        try:
+            fpath = '%s/%s.mp3' % (output_path, title)
+            #pprint(fpath)
+            if not os.path.exists(fpath):
+                tmp_file = "%s.part" % fpath
+
+                if os.path.exists(tmp_file):
+                    print ("- [r] temp file %s" % tmp_file)
+                    os.remove(tmp_file)
+
+                print ("+ [a] %s" % title)
+                urlretrieve(url, tmp_file)
+                os.rename(tmp_file, fpath)
+
+            else:
+                print ("> [s] %s" % title)
+
+        except Exception as err:
+            print(err)
+
+        queue.task_done()
+
+#===============================================================================
+def safe_fs_name(name):
+    """ Получить имя подходящее для сохранения файлов и папок """
+
+    name = html.unescape(name)
+
+    newname = ''
+    for c in name:
+        newname += c if (c.isalnum() or c in keepcharacters) else '-'
+
+    newname = "".join(newname).strip()
+    newname = re.sub(r'\-+', '-', newname)
+    newname = re.sub(r'\s?\-\s?', '-', newname)
+    newname = re.sub(r'\-$', '', newname)
+
+    return newname
+
+#===============================================================================
 # public static void
+#===============================================================================
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("action", type=str, help="Действие: parse|download")
     parser.add_argument("config", type=str, help="Файл с параметрами входа на ВК")
     parser.add_argument("output", type=str, help="Куда сохранить результаты")
     parser.add_argument("-v", "--verbose", help="Подробные сообщения", action="store_true", default=True)
     parser.add_argument("-e", "--every", help="Пауза каждые n треков", type=int, default=200)
-    parser.add_argument("-p", "--pause", help="Продолжительность паузы", type=int, default=15)
+    parser.add_argument("-p", "--pause", help="Продолжительность паузы, секунды", type=int, default=15)
+    parser.add_argument("-t", "--threads", help="Количество процессов на закачку", type=int, default=5)
 
     args = parser.parse_args()
-    main(args.config, args.output, args.verbose, args.every, args.pause)
 
-#print("".join(c for c in album['atitle'] if c.isalnum() or c in keepcharacters).rstrip())
+    if args.action == 'download':
+        download(args.config, args.output, args.verbose, args.threads, args.pause)
+    elif args.action == 'parse':
+        parse(args.config, args.output, args.verbose, args.every, args.pause)
+    else:
+        raise Exception('Неизвестное дейтсвие')
+
 
